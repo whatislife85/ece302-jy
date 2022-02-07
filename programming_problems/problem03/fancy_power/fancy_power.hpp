a8dda64b2cf1d539de3ede6866d037c748fd2827
jgy5 <jgy5@pitt.edu>
2022-02-06 23:58:53 -0500

    did hw3

--- programming_problems/problem03/fancy_power/fancy_power.hpp
+++ programming_problems/problem03/fancy_power/fancy_power.hpp
@@ -1,10 +1,16 @@
 #ifndef FANCYPOWER_HPP
 #define FANCYPOWER_HPP
 
-int fancy_power(int n, int m)
+int fancy_power(int n, int m) //returns n^m
 {
-	// your code should go here.
-	return 0;
+	int ans;
+	
+	if (m == 0) {ans = 1;}
+	if (m == 1) {ans = n;}
+	else if (m % 2) {ans = (n * fancy_power(n*n, (m-1)/2));}
+	else {ans = fancy_power(n*n, m/2);}
+
+	return ans;
 }
 
 #endif
--- programming_problems/problem03/fancy_power/fancy_power_tests.cpp
+++ programming_problems/problem03/fancy_power/fancy_power_tests.cpp
@@ -2,10 +2,16 @@
 #include "catch.hpp"
 
 #include "fancy_power.hpp"
+#include <math.h>
 
 // write your test cases here
 TEST_CASE("Test case", "fancy power"){
-
-  REQUIRE(true);
+  /*for (int i = 0; i < 6; i++) {
+    for(int j = 0; j < 6; j++) {
+	REQUIRE(fancy_power(i, j) == pow(i, j));
+    }
+  }*/
+  REQUIRE(fancy_power(2, 10) == pow(2, 10));
+  //REQUIRE(true);
   
 }
